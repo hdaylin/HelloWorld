2d362f988ca74654c90ddb429929fed335b33af4
hdaylin <hdaylin@yahoo.com>
2017-05-24 13:26:19 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+		/* This is a multi line comment
+		 * Author: Daylin 
+		 * this program is a HelloWorld 
+		 */
+	
+	public static void main(String[] args) {
+		// This is a inline comment 
+		System.out.println("Hello, World");
+
+	}
+
+}
